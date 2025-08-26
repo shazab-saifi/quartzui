1ee0a778bd188338e89f4cd8da8fd62d90dcdff2
shazab-saifi <shazebsaifi92@gmail.com>
2025-08-26 15:45:42 +0530

    Added gradient and destructive variants in button component

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,8 +1,10 @@
 import { cn } from '@/lib/utils';
 import { tv } from 'tailwind-variants';
+import { LuLoaderCircle } from 'react-icons/lu';
 
 export interface ButtonProps {
-  variant?: 'primary' | 'secondary';
+  variant?: 'primary' | 'secondary' | 'gradient' | 'destructive';
+  loader?: boolean;
   className?: string;
   size?: 'sm' | 'md' | 'lg';
   label: string;
@@ -10,11 +12,14 @@ export interface ButtonProps {
 }
 
 const buttonVariants = tv({
-  base: 'font-medium bg-blue-500 text-white rounded-lg active:opacity-80 transition-colors cursor-pointer',
+  base: 'font-medium bg-blue-500 text-white rounded-lg active:opacity-80 transition-colors cursor-pointer inline-flex gap-2 items-center justify-center',
   variants: {
     color: {
-      primary: 'bg-black text-white hover:bg-neutral-900',
+      primary: 'bg-black hover:bg-neutral-900 border-1 border-neutral-800',
       secondary: 'bg-white text-black hover:bg-neutral-200',
+      gradient:
+        'bg-linear-65 from-purple-500 to-pink-500 rounded-full font-semibold hover:ring-1',
+      destructive: 'bg-red-500 hover:bg-red-400',
     },
     size: {
       sm: 'text-sm',
@@ -40,13 +45,28 @@ export const Button = ({
   size,
   label,
   onClick,
+  loader,
+  ...props
 }: ButtonProps) => {
+  const loaderSizeMap: Record<'sm' | 'md' | 'lg', number> = {
+    sm: 16,
+    md: 18,
+    lg: 20,
+  };
+  const loaderIconSize = loaderSizeMap[size ?? 'md'];
+
   return (
     <button
       type="button"
       onClick={onClick}
       className={cn(buttonVariants({ size, color: variant }), className)}
+      {...props}
     >
+      {loader && (
+        <span className="animate-spin">
+          <LuLoaderCircle size={loaderIconSize} />
+        </span>
+      )}
       {label}
     </button>
   );
