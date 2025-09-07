f0d582226339e2de875f5909fbbbe5c9cf3ce536
shazab-saifi <shazebsaifi92@gmail.com>
2025-09-07 17:21:39 +0530

    set up prisma

---
+++ prisma/migrations/20250907114435_create_user_table/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "provider" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
