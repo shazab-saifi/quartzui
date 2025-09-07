a47f82a472897d9670871d281e21f3f983d06455
shazab-saifi <shazebsaifi92@gmail.com>
2025-09-07 17:26:38 +0530

    Create a prisma singleton

---
+++ lib/prismaClient.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from '@prisma/client';
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+if (!global.prisma) {
+  global.prisma = new PrismaClient({
+    log: ['info'],
+  });
+}
+
+const prisma: PrismaClient = global.prisma;
+
+export default prisma;
