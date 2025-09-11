c9a0697eaaa8bb5c7374062b2feba80bf69089ec
shazab-saifi <shazebsaifi92@gmail.com>
2025-09-11 13:21:17 +0530

    Created a about card in components page. with owner intro and project description

--- app/components/page.tsx
+++ app/components/page.tsx
@@ -2,6 +2,7 @@ import ComponentsBar from '@/ui/componentsPage/ComponentsBar';
 // import ButtonMdx from '@/docs/button.mdx';
 import AnimatedButtonMdx from '@/docs/animated-button.mdx';
 import DocsLayout from '@/docs/layout';
+import { Button } from '@/components/Button';
 
 const page = () => {
   return (
@@ -16,7 +17,24 @@ const page = () => {
           <AnimatedButtonMdx />
         </DocsLayout>
       </div>
-      <div className="sticky top-30 h-96 w-64 bg-neutral-900"></div>
+      <div className="space-y-sm sticky top-30 h-fit max-w-54 rounded-lg bg-conic-180 [background-image:conic-gradient(from_180deg_at_0%_0%,theme(colors.indigo.600),theme(colors.neutral.700),theme(colors.neutral.900))] from-neutral-800 to-neutral-900 [background-position:top_left] [background-origin:padding-box] p-4">
+        <p className="text-para font-semibold text-neutral-200">
+          Hii, I&apos;m Shazab. I&apos;m the creater of Quartz UI.
+        </p>
+        <div className="space-y-md">
+          <p className="text-small text-neutral-600 dark:text-neutral-400">
+            Quartz UI is a modern React component library focused on simplicity,
+            accessibility, and beautiful design.
+          </p>
+          <Button
+            size="sm"
+            variant="secondary"
+            className="rounded-xl bg-neutral-200 font-normal text-black hover:bg-neutral-300"
+          >
+            Follor on 𝕏
+          </Button>
+        </div>
+      </div>
     </div>
   );
 };
