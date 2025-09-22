226dbd0ae0d48979835e24fabce9a36b05d76747
shazab-saifi <shazebsaifi92@gmail.com>
2025-09-22 21:48:59 +0530

    Made pricing page light and dark mode compatible

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -14,59 +14,76 @@ const PricingCard = ({ plan }: { plan: PlanType }) => {
   const isProfessional = plan.title === 'Professional';
   const isEnterprise = plan.title === 'Enterprise';
 
-  let bgGradient;
+  let bgGradient =
+    'bg-[radial-gradient(circle_at_top_right,theme(colors.neutral.100),theme(colors.white),theme(colors.white),theme(colors.white))] dark:bg-[radial-gradient(circle_at_top_right,theme(colors.neutral.700),theme(colors.neutral.950),theme(colors.neutral.950),theme(colors.neutral.950))]';
+
   if (isProfessional) {
     bgGradient =
-      'bg-[radial-gradient(circle_at_top,theme(colors.neutral.700),theme(colors.neutral.950),theme(colors.neutral.950))]';
+      'bg-[radial-gradient(circle_at_top,theme(colors.neutral.100),theme(colors.white),theme(colors.white))] dark:bg-[radial-gradient(circle_at_top,theme(colors.neutral.700),theme(colors.neutral.950),theme(colors.neutral.950))]';
   } else if (isEnterprise) {
     bgGradient =
-      'bg-[radial-gradient(circle_at_top_left,theme(colors.neutral.700),theme(colors.neutral.950),theme(colors.neutral.950),theme(colors.neutral.950))]';
-  } else {
-    bgGradient =
-      'bg-[radial-gradient(circle_at_top_right,theme(colors.neutral.700),theme(colors.neutral.950),theme(colors.neutral.950),theme(colors.neutral.950))]';
+      'bg-[radial-gradient(circle_at_top_left,theme(colors.neutral.100),theme(colors.white),theme(colors.white),theme(colors.white))] dark:bg-[radial-gradient(circle_at_top_left,theme(colors.neutral.700),theme(colors.neutral.950),theme(colors.neutral.950),theme(colors.neutral.950))]';
   }
 
   return (
     <div
-      className={`mx-auto flex w-84 flex-col justify-between gap-20 rounded-2xl border border-neutral-200 md:rounded-3xl ${bgGradient} p-4 md:min-h-[800px] md:w-92 md:p-6 dark:border-neutral-800`}
+      className={`mx-auto flex w-84 flex-col justify-between gap-20 rounded-2xl border border-neutral-200 transition-colors md:rounded-3xl ${bgGradient} p-4 md:min-h-[800px] md:w-92 md:p-6 dark:border-neutral-800`}
     >
       <div className="space-y-9">
         <div className="space-y-md md:space-y-6">
           <div
-            className={`text-lg font-semibold ${isProfessional ? 'text-neutral-100' : 'text-neutral-400'}`}
+            className={`text-lg font-semibold transition-colors ${
+              isProfessional
+                ? 'text-neutral-950 dark:text-neutral-100'
+                : 'text-neutral-700 dark:text-neutral-400'
+            }`}
           >
             {plan.title}
           </div>
           <div>
-            <span className="text-4xl font-semibold md:text-5xl">
+            <span className="text-4xl font-semibold text-neutral-950 transition-colors md:text-5xl dark:text-neutral-100">
               ${plan.price}
             </span>
-            <span className="text-xs text-neutral-500 md:text-sm dark:text-neutral-400">
+            <span className="text-xs text-neutral-500 transition-colors md:text-sm dark:text-neutral-400">
               /month
             </span>
           </div>
-          <p className="text-sm text-neutral-600 dark:text-neutral-400">
+          <p
+            className={`text-sm transition-colors ${isProfessional ? 'text-neutral-800 dark:text-neutral-100' : 'text-neutral-600 dark:text-neutral-400'}`}
+          >
             {plan.desc}
           </p>
         </div>
-        <div className="flex items-center justify-center gap-2 text-xs text-neutral-500 dark:text-neutral-700">
-          <span className="h-[2px] w-full bg-neutral-300 dark:bg-neutral-700"></span>
-          <span>Includes</span>
-          <span className="h-[2px] w-full bg-neutral-300 dark:bg-neutral-700"></span>
+        <div className="flex items-center justify-center gap-2 text-xs text-neutral-500 transition-colors dark:text-neutral-400">
+          <span className="h-[2px] w-full bg-neutral-300 transition-colors dark:bg-neutral-700"></span>
+          <span className="transition-colors">Includes</span>
+          <span className="h-[2px] w-full bg-neutral-300 transition-colors dark:bg-neutral-700"></span>
         </div>
         <div className="space-y-4">
           {plan.offerings.map((item, idx) => (
             <div
               key={idx}
-              className={`flex gap-2 text-sm ${isProfessional ? 'text-neutral-100' : 'text-neutral-400'}`}
+              className={`flex gap-2 text-sm transition-colors ${
+                isProfessional
+                  ? 'text-neutral-950 dark:text-neutral-100'
+                  : 'text-neutral-700 dark:text-neutral-400'
+              }`}
             >
-              <IconCircleCheckFilled />
-              <p>{item}</p>
+              <IconCircleCheckFilled className="transition-colors" />
+              <p className="transition-colors">{item}</p>
             </div>
           ))}
         </div>
       </div>
-      <Button size="sm" variant={isProfessional ? 'secondary' : 'primary'}>
+      <Button
+        variant={isProfessional ? 'secondary' : 'primary'}
+        className={
+          (isProfessional
+            ? 'border-none bg-neutral-950 text-neutral-100 hover:bg-neutral-800 hover:text-neutral-50 dark:bg-gradient-to-b dark:from-neutral-100 dark:to-neutral-300 dark:text-neutral-950 dark:hover:from-neutral-200 dark:hover:to-neutral-400 dark:hover:text-neutral-900'
+            : 'border-none bg-neutral-100 text-neutral-950 hover:bg-neutral-200 hover:text-neutral-900 dark:bg-neutral-900 dark:text-neutral-100 dark:hover:bg-neutral-800 dark:hover:text-neutral-50') +
+          ' transition-colors'
+        }
+      >
         Get {plan.title} Now
       </Button>
     </div>
@@ -150,19 +167,19 @@ const page = () => {
   ];
 
   return (
-    <div className="mt-16 space-y-24 md:space-y-32 lg:space-y-40 xl:space-y-50">
+    <div className="mt-16 space-y-24 bg-white transition-colors md:space-y-32 lg:space-y-40 xl:space-y-50 dark:bg-neutral-950">
       <div className="space-y-16 md:space-y-20">
         <div className="flex w-full flex-col items-center gap-6 md:gap-8">
-          <h1 className="xl:text-h1 md:text-h3 lg:text-h2 max-w-4xl text-center text-3xl font-bold">
+          <h1 className="xl:text-h1 md:text-h3 lg:text-h2 max-w-4xl text-center text-3xl font-bold text-neutral-950 transition-colors dark:text-neutral-100">
             Simple, Transparent Pricing Built for Every Team
           </h1>
-          <p className="md:text-para lg:text-para-big max-w-3xl text-center text-sm text-neutral-600 dark:text-neutral-400">
+          <p className="md:text-para lg:text-para-big max-w-3xl text-center text-sm text-neutral-600 transition-colors dark:text-neutral-400">
             From solo devs to enterprises, we’ve got you covered.
           </p>
         </div>
         <div className="w-full space-y-8 md:space-y-12">
           <TabSelect tabs={['Monthly', 'Yearly']} />
-          <div className="flex w-full flex-wrap gap-16">
+          <div className="flex w-full flex-wrap gap-16 transition-colors">
             {plans.map((plan) => (
               <PricingCard key={plan.title} plan={plan} />
             ))}
@@ -170,7 +187,7 @@ const page = () => {
         </div>
       </div>
       <div className="mt-32 space-y-12 md:space-y-16">
-        <h1 className="md:text-h4 text-h5 text-center leading-7 font-semibold">
+        <h1 className="md:text-h4 text-h5 text-center leading-7 font-semibold text-neutral-950 transition-colors dark:text-neutral-100">
           Frequently Asked Questions
         </h1>
         <FAQ faqs={faqs} />
--- ui/pricing/FAQ.tsx
+++ ui/pricing/FAQ.tsx
@@ -15,17 +15,17 @@ const FAQ = ({ faqs }: { faqs: { qes: string; ans: string }[] }) => {
       {faqs.map((item, idx) => (
         <div
           key={idx}
-          className="mx-auto max-w-192 border-b border-neutral-200 pb-6 last:border-b-0 dark:border-white/10"
+          className="mx-auto max-w-192 border-b border-neutral-200 pb-6 transition-colors last:border-b-0 dark:border-white/10"
         >
           <button
-            className="flex w-full cursor-pointer items-center justify-between py-3 text-left text-neutral-900 transition focus:outline-none focus-visible:ring-2 focus-visible:ring-neutral-300 dark:text-white dark:focus-visible:ring-white/30"
+            className="flex w-full cursor-pointer items-center justify-between py-3 text-left text-neutral-900 transition-colors focus:outline-none focus-visible:ring-2 focus-visible:ring-neutral-300 dark:text-white dark:focus-visible:ring-white/30"
             onClick={() => handleToggle(idx)}
             aria-expanded={openIndex === idx}
             aria-controls={`accordion-content-${idx}`}
             id={`accordion-header-${idx}`}
             type="button"
           >
-            <span className="text-lg">{item.qes}</span>
+            <span className="text-lg transition-colors">{item.qes}</span>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="24"
@@ -36,7 +36,7 @@ const FAQ = ({ faqs }: { faqs: { qes: string; ans: string }[] }) => {
               stroke-width="2"
               stroke-linecap="round"
               stroke-linejoin="round"
-              className="icon icon-tabler icons-tabler-outline icon-tabler-plus shrink-0"
+              className="icon icon-tabler icons-tabler-outline icon-tabler-plus shrink-0 transition-colors"
             >
               <path stroke="none" d="M0 0h24v24H0z" />
               <path
@@ -76,10 +76,10 @@ const FAQ = ({ faqs }: { faqs: { qes: string; ans: string }[] }) => {
                   duration: 0.35,
                   ease: [0.4, 0, 0.2, 1],
                 }}
-                className="overflow-hidden"
+                className="overflow-hidden transition-colors"
                 style={{ willChange: 'height, opacity, padding' }}
               >
-                <div className="py-2 text-neutral-700 dark:text-neutral-200">
+                <div className="py-2 text-neutral-700 transition-colors dark:text-neutral-200">
                   {item.ans}
                 </div>
               </motion.div>
