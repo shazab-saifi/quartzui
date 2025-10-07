e3481de7143195475ccc082c807ec34138d08e15
shazab-saifi <shazebsaifi92@gmail.com>
2025-10-07 12:29:48 +0530

    Added paddingright on body when the dialog open so it doesn't feel like that the whole body is expending to righ to fill the space when scroll bar disappears

--- components/Dialog.tsx
+++ components/Dialog.tsx
@@ -20,13 +20,16 @@ export const Dialog = ({
     };
 
     if (isOpen) {
-      document.addEventListener('keydown', handleKeyDown);
+      const scrollbarWidth =
+        window.innerWidth - document.documentElement.clientWidth;
       document.body.style.overflow = 'hidden';
+      document.body.style.paddingRight = `${scrollbarWidth}px`;
     }
 
     return () => {
       document.removeEventListener('keydown', handleKeyDown);
       document.body.style.overflow = '';
+      document.body.style.paddingRight = '0px';
     };
   }, [isOpen]);
 
