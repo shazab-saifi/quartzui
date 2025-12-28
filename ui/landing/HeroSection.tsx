28ddbb716d0b63a6a0fb049ef8f2ced9ab01d393
shazab-saifi <shazebsaifi92@gmail.com>
2025-12-28 11:09:22 +0530

    Added perspective images with animations

--- ui/landing/HeroSection.tsx
+++ ui/landing/HeroSection.tsx
@@ -1,66 +1,42 @@
-'use client';
-
 import { Button } from '@/components/Button';
 import TextShimmer from '@/components/TextShimmer';
-import {
-  IconBrandFramerMotion,
-  IconBrandGithub,
-  IconBrandNextjs,
-  IconBrandReact,
-  IconBrandStorybook,
-  IconBrandTailwind,
-} from '@tabler/icons-react';
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
-import { motion } from 'motion/react';
 import AnimatedText from '@/components/AnimatedText';
-import Marquee from 'react-fast-marquee';
 import { Heading } from './heading';
-
-interface ItemType {
-  icon: React.ElementType;
-  title: string;
-}
-
-const items: ItemType[] = [
-  { icon: IconBrandReact, title: 'React.js' },
-  { icon: IconBrandFramerMotion, title: 'Motion.dev' },
-  { icon: IconBrandNextjs, title: 'Next.js' },
-  { icon: IconBrandStorybook, title: 'Storybook.js' },
-  { icon: IconBrandGithub, title: 'Github' },
-  { icon: IconBrandTailwind, title: 'Tailwind.css' },
-];
+import HeroImages from './hero-images';
+import Motion from '../motion';
+import Link from 'next/link';
 
 const HeroSection = () => {
-  const router = useRouter();
-
   return (
-    <div className="mt-16 flex flex-col items-center overflow-hidden md:mt-28">
-      <div className="z-10 flex flex-col items-center gap-4 md:gap-6">
-        <motion.button
-          initial={{ opacity: 0, filter: 'blue(10px)', y: 10 }}
-          animate={{ opacity: 1, filter: 'blue(0px)', y: 0 }}
-          onClick={() => router.push('/components')}
-          className="mb-4 flex cursor-pointer items-center gap-2 rounded-full border border-neutral-200 bg-neutral-100 px-2 py-1 text-xs dark:border-neutral-800 dark:bg-neutral-900"
-        >
-          <Image
-            src="https://res.cloudinary.com/dlpjh3fcx/image/upload/v1759296296/quartzui-light_ytrk9a.svg"
-            alt="logo"
-            width={12}
-            height={12}
-            className="hidden dark:inline"
-          />
-          <Image
-            src="https://res.cloudinary.com/dlpjh3fcx/image/upload/v1759296311/quartzui-dark_ixfmns.svg"
-            alt="logo"
-            width={12}
-            height={12}
-            className="inline dark:hidden"
-          />
-          <TextShimmer>
-            Beautiful components. Effortless experience. »
-          </TextShimmer>
-        </motion.button>
+    <div className="mt-16 flex flex-col items-center md:mt-28">
+      <div className="z-10 flex flex-col items-center gap-4 sm:px-8 md:gap-6">
+        <Link href="/docs">
+          <Motion
+            as="button"
+            initial={{ opacity: 0, filter: 'blue(10px)', y: 10 }}
+            animate={{ opacity: 1, filter: 'blue(0px)', y: 0 }}
+            className="mb-4 flex cursor-pointer items-center gap-2 rounded-full border border-neutral-200 bg-neutral-100 px-2 py-1 text-xs dark:border-neutral-800 dark:bg-neutral-900"
+          >
+            <Image
+              src="https://res.cloudinary.com/dlpjh3fcx/image/upload/v1759296296/quartzui-light_ytrk9a.svg"
+              alt="logo"
+              width={12}
+              height={12}
+              className="hidden dark:inline"
+            />
+            <Image
+              src="https://res.cloudinary.com/dlpjh3fcx/image/upload/v1759296311/quartzui-dark_ixfmns.svg"
+              alt="logo"
+              width={12}
+              height={12}
+              className="inline dark:hidden"
+            />
+            <TextShimmer>
+              Beautiful components. Effortless experience. »
+            </TextShimmer>
+          </Motion>
+        </Link>
         <div className="max-w-2xl">
           <Heading as="h1">
             <AnimatedText>
@@ -68,7 +44,8 @@ const HeroSection = () => {
             </AnimatedText>
           </Heading>
         </div>
-        <motion.p
+        <Motion
+          as="p"
           initial={{ opacity: 0, filter: 'blue(10px)', y: 10 }}
           animate={{ opacity: 1, filter: 'blue(0px)', y: 0 }}
           transition={{ duration: 0.3, delay: 0.6 }}
@@ -77,45 +54,28 @@ const HeroSection = () => {
           Our UI library gives you elegant, production-ready components built
           for speed, flexibility, and creativity. Design smarter, ship faster,
           and focus on what truly matters your product.
-        </motion.p>
+        </Motion>
       </div>
-      <motion.div
+      <Motion
         initial={{ opacity: 0, filter: 'blue(10px)', y: 10 }}
         animate={{ opacity: 1, filter: 'blue(0px)', y: 0 }}
         transition={{ duration: 0.3, delay: 0.8 }}
         className="z-10 mt-12 space-x-4 md:mt-14 md:space-x-6"
       >
-        <Button
-          variant="secondary"
-          onClick={() => router.push('/components')}
-          className="px-6 text-sm"
+        <Link href="/docs">
+          <Button variant="secondary" className="px-6 text-sm">
+            Components
+          </Button>
+        </Link>
+        <a
+          href="https://x.com/shazabsaifi_s9"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          Components
-        </Button>
-        <Button
-          onClick={() =>
-            (window.location.href = 'https://x.com/shazabsaifi_s9')
-          }
-          className="px-5 text-sm"
-        >
-          Request component
-        </Button>
-      </motion.div>
-      <div className="mt-16 flex justify-between gap-4 overflow-hidden mask-r-from-80% mask-l-from-80% sm:w-[80%] md:mt-24 lg:mt-32">
-        <Marquee>
-          <div className="flex justify-between gap-16">
-            {items.map(({ title, icon: Icon }, idx) => (
-              <div
-                key={idx}
-                className="mx-auto flex items-center gap-2 text-neutral-600 dark:text-neutral-400"
-              >
-                <Icon className="size-4 md:size-6" />
-                <span className="md:text-lg">{title}</span>
-              </div>
-            ))}
-          </div>
-        </Marquee>
-      </div>
+          <Button className="px-5 text-sm">Request component</Button>
+        </a>
+      </Motion>
+      <HeroImages />
     </div>
   );
 };
---
+++ ui/landing/hero-images.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import Image from 'next/image';
+// import { motion } from 'motion/react';
+import useResolvedTheme from '@/hooks/useResolvedTheme';
+import Motion from '../motion';
+
+const HERO_IMG_DARK_1 =
+  'https://res.cloudinary.com/dlpjh3fcx/image/upload/v1766669805/quartzui-hero-image-2_gufuqf.png';
+const HERO_IMG_DARK_2 =
+  'https://res.cloudinary.com/dlpjh3fcx/image/upload/v1766669803/quartzui-hero-image-1_dgal3y.png';
+const HERO_IMG_LIGHT =
+  'https://res.cloudinary.com/dlpjh3fcx/image/upload/v1766759127/hero-image-light_vo1ny8.webp';
+
+const HeroImages = () => {
+  const resolvedTheme = useResolvedTheme();
+  const isDark = resolvedTheme === 'dark';
+
+  return (
+    <div className="relative min-h-200 w-full pt-20">
+      <div
+        aria-hidden="true"
+        className="pointer-events-none absolute -inset-10 -bottom-2 left-0 z-10 bg-linear-to-t from-white from-30% to-transparent to-50% dark:from-neutral-950"
+      />
+      <Motion
+        initial={{ opacity: 0, y: -100, x: 100 }}
+        animate={{ opacity: 1, y: 0, x: 0 }}
+        transition={{ duration: 1.5, delay: 1, ease: [0.16, 1, 0.6, 1] }}
+        className="[filter:drop-shadow(0_20px_40px_rgba(0,0,0,0.20))] perspective-[4000px]"
+      >
+        <Image
+          src={isDark ? HERO_IMG_DARK_1 : HERO_IMG_LIGHT}
+          alt="hero-image-1"
+          height={1080}
+          width={1020}
+          className="absolute translate-x-[10%] rounded-lg border border-neutral-200 dark:border-neutral-800"
+          style={{
+            transform: 'rotateY(20deg) rotateX(20deg) rotateZ(-5deg)',
+          }}
+        />
+      </Motion>
+      <Motion
+        initial={{ opacity: 0, y: -100, x: 100 }}
+        animate={{ opacity: 1, y: 0, x: 0 }}
+        transition={{ duration: 1.5, delay: 1.3, ease: [0.16, 1, 0.6, 1] }}
+        className="[filter:drop-shadow(0_20px_40px_rgba(0,0,0,0.20))] perspective-[4000px]"
+      >
+        <Image
+          src={isDark ? HERO_IMG_DARK_2 : HERO_IMG_LIGHT}
+          alt="hero-image-2"
+          height={1080}
+          width={1020}
+          className="absolute translate-x-[40%] translate-y-12 rounded-lg border border-neutral-200 dark:border-neutral-800"
+          style={{
+            transform: 'rotateY(20deg) rotateX(20deg) rotateZ(-5deg)',
+          }}
+        />
+      </Motion>
+    </div>
+  );
+};
+
+export default HeroImages;
